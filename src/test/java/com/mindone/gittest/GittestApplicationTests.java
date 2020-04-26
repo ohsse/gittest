6ae1312870bd379ed7ee63a74a6e308c6406f26e
SJOH\e-matrix_clee <opure0714@gmail.com>
2020-04-27 08:37:32 +0900

    Initial commit

---
+++ src/test/java/com/mindone/gittest/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindone.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
